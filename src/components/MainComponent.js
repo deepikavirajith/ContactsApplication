e2769c349453478cddce61dd97b1714fff357ca2
Lakshmi Deepika Dodda <lakshmi.dodda@pollub.edu.pl>
2025-04-24 12:26:34 +0200

    Initial commit for new repo

---
+++ src/components/MainComponent.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+
+
+
+export const baseUrl = "http://localhost:3001/data";
+
+
+const MainComponent = () => {
+
+    const [state, setState] = useState({
+        contacts: {
+            pname: '',
+            pemail: '',
+            pnumber: '',
+            islogged: false
+        }
+    });
+
+
+    const navigate = useNavigate();
+
+    const createData = async (e) => {
+        e.preventDefault();
+        const response = await axios.post(baseUrl, {
+            pname: state.contacts.pname,
+            pemail: state.contacts.pemail,
+            pnumber: state.contacts.pnumber
+        });
+        console.log(response);
+        if (!state.contacts.pname || !state.contacts.pemail || !state.contacts.pnumber) {
+            return toast.warning('Please fill all the fields');
+        }
+
+        else {
+            toast.success('contact added', {
+                autoClose: 1000
+            });
+            setState({
+                contacts: {
+                    pname: '',
+                    pemail: '',
+                    pnumber: '',
+                    islogged: true
+                }
+            });
+            navigate('/');
+        }
+
+
+    }
+
+
+    const handleOnchange = (e) => {
+        e.preventDefault();
+        setState({
+            contacts: {
+                ...state.contacts,
+                [e.target.name]: e.target.value
+            }
+        });
+    }
+
+
+    return (
+        <div className="container">
+            <h1 className="display-4 my-4 text-center">Add Contact</h1>
+            <div className="row">
+                <div className="col-sm-6 offset-sm-3">
+                    <form onSubmit={createData}>
+                        <div className="form-group">
+                            <input className="form-control" type='text' placeholder='Enter Name' name='pname'
+                                value={state.contacts.pname} onChange={handleOnchange} required={true}></input>
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control" type='email' placeholder='Enter Email' name='pemail'
+                                value={state.contacts.pemail} onChange={handleOnchange} required={true}></input>
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control" type='mobile' placeholder='Enter Number' name='pnumber'
+                                value={state.contacts.pnumber} onChange={handleOnchange} required={true}></input>
+                        </div>
+                        <button type="submit" className="btn btn-dark btn-block">
+                            Add Contact
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+
+
+}
+export default MainComponent;
---
+++ src/components/SearchComponent.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import ContactList from './ContactList';
+import { Link } from 'react-router-dom';
+
+const SearchComponent = () => {
+    const contacts = useSelector((state) => state.contacts.contacts);
+    console.log(contacts);
+
+    const [searchQuery, setSearchQuery] = useState('');
+    // Handle the case when contacts is null or not yet fetched
+    if (contacts === null) {
+        return <div>Loading contacts...</div>;
+    }
+
+    console.log(contacts);
+    const searchbtn = contacts.filter((contact) => {
+        return contact.pname.toLowerCase().includes(searchQuery.toLowerCase());
+    });
+
+    const handleSearchInputChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    if (contacts === null) {
+        return <div>Loading contacts...</div>;
+    }
+
+
+    return (
+        <div className='container'>
+            <div className="row mt-4">
+                <div className='col text-center'>
+                    <form>
+                        <input
+                            type='text'
+                            placeholder='Search contacts...'
+                            value={searchQuery}
+                            onChange={handleSearchInputChange}
+                        />
+                    </form>
+                </div>
+                <div className='col text-center'>
+                    <Link to="/main" className='btn btn-dark'>
+                        Add Contact
+                    </Link>
+                </div>
+            </div>
+            <div className='row'>
+                {searchbtn.map((contact) => (
+                    <div key={contact.id}>
+                        <ContactList contact={contact} />
+                    </div>
+                ))}
+            </div>
+        </div>
+
+
+    );
+}
+
+export default SearchComponent;
